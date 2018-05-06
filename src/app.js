3120e245cf079bddfc1eff5dd42246c9bb318785
Chris Mah <chrismah@cmah-2.local>
2018-05-06 14:32:14 -0400

    Commented dummy expense data

--- src/app.js
+++ src/app.js
@@ -13,15 +13,14 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'water bill', amount: 100}));
-store.dispatch(addExpense({description: 'gas bill', amount: 200, createdAt: 1000}));
-store.dispatch(addExpense({description: 'rent', amount: 109500}));
+//store.dispatch(addExpense({description: 'water bill', amount: 100}));
+//store.dispatch(addExpense({description: 'gas bill', amount: 200, createdAt: 1000}));
+//store.dispatch(addExpense({description: 'rent', amount: 109500}));
 //store.dispatch(setTextFilter('gas'));
 
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+//const state = store.getState();
+//const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//console.log(visibleExpenses);
 
 
 //console.log(store.getState());
